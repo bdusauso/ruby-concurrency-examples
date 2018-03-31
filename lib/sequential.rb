cfc85bcc17bdb93f9273ad2a62bce59cbcbc6b00
Bruno Dusausoy <bruno.dusausoy@gmail.com>
2018-03-31 22:08:54 +0200

    Use TemperatureFetcher class

--- lib/sequential.rb
+++ lib/sequential.rb
@@ -1,42 +1,15 @@
-require "rest-client"
-require "concurrent"
 require "benchmark"
-require "json"
 
-API_KEY  = "ce58792015f3118e4da9a8f26a318498"
-BASE_URL = "https://api.openweathermap.org/data/2.5/weather"
-
-DEFAULT_PARAMS = {
-  units: :metric,
-  APPID: API_KEY
-}
-
-CITIES = %w(
-  Brussels
-  Paris
-  London
-  Berlin
-  Stockholm
-  Madrid
-  Tirana
-  Yerevan
-  Vienna
-  Baku
-  Bucharest
-)
+require_relative "./temperature_fetcher.rb"
+require_relative "./cities.rb"
 
 time = Benchmark.measure do
-  CITIES.each do |city|
-    begin
-      response = RestClient.get(BASE_URL, params: DEFAULT_PARAMS.merge({q: city}))
-    rescue RestClient::NotFound => err
-      puts "No data found for #{city}"
-    else
-      content = JSON.parse(response.body)
-      temp    = content["main"]["temp"]
-      
-      puts "#{city}: #{temp}" 
-    end
+  temperatures = CITIES.map do |city|
+    TemperatureFetcher.new(city).fetch
+  end
+
+  temperatures.each do |temp|
+    puts "#{temp.first}: #{temp.last}"
   end
 end
 
