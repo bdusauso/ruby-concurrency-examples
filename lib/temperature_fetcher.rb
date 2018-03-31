923785626cbcbd3df3683b714352e534f56e9ad3
Bruno Dusausoy <bruno.dusausoy@gmail.com>
2018-03-31 22:08:12 +0200

    Extract temperature fetcher in its own class

---
+++ lib/temperature_fetcher.rb
@@ -0,0 +1,30 @@
+require "rest-client"
+require "json"
+
+class TemperatureFetcher
+
+  API_KEY  = "ce58792015f3118e4da9a8f26a318498"
+  BASE_URL = "https://api.openweathermap.org/data/2.5/weather"
+
+  DEFAULT_PARAMS = {
+    units: :metric,
+    APPID: API_KEY
+  }
+
+  def initialize(city)
+    super()
+    @city = city
+  end
+
+  def fetch
+    begin
+      response = RestClient.get(BASE_URL, params: DEFAULT_PARAMS.merge({ q: @city }))
+    rescue RestClient::NotFound
+      puts "No data found for #{city}"
+    else
+      content = JSON.parse(response.body)
+      [@city, content["main"]["temp"]]
+    end
+  end
+
+end
