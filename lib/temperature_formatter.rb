4fffb2a2b6e25d7afc9fdc4135549561756170a5
Bruno Dusausoy <bruno.dusausoy@gmail.com>
2018-04-01 09:07:18 +0200

    Improve formatting

--- lib/temperature_formatter.rb
+++ lib/temperature_formatter.rb
@@ -3,7 +3,10 @@ require 'terminal-table'
 class TemperatureFormatter
 
   def self.format(temperatures)
-    Terminal::Table.new :rows => temperatures
+    table = Terminal::Table.new :rows => temperatures, :headings => %w(City °C)
+    table.align_column(1, :right)
+
+    table
   end
 
 end
