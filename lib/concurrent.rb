b096fcaa2cb479f34bfcb8c4e3270fc0f8ceb92e
Bruno Dusausoy <bruno.dusausoy@gmail.com>
2018-04-01 16:32:57 +0200

    Use a fixed thread pool of 10 threads

--- lib/concurrent.rb
+++ lib/concurrent.rb
@@ -7,10 +7,11 @@ require_relative "./cities.rb"
 
 # Just so 'temperatures' exists and is not nil
 temperatures = []
+pool         = Concurrent::FixedThreadPool.new(10)
 
 time = Benchmark.measure do
   temperatures = CITIES.map do |city|
-    Concurrent::Future.execute { TemperatureFetcher.new(city).fetch }
+    Concurrent::Future.execute(executor: pool) { TemperatureFetcher.new(city).fetch }
   end
 
   until temperatures.all?(&:fulfilled?) do
